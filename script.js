ef50f431414354688f965f5dfb01d4e3e51d6149
Kar0r404 <mahlarekarabo.702@gmail.com>
2025-03-23 03:56:07 +0200

    show more text

--- script.js
+++ script.js
@@ -13,14 +13,20 @@ function loadComponent(elementId, filePath) {
 loadComponent("footer", "./footer.html");
 loadComponent("navbar", "./navbar.html");
 
-function useNavBar() {
-    fetch('./navbar.html')
-        .then(response => 
-            {
-                console.log(response.text)
-                return response.text()})
-        .then(data => {
-            document.getElementById('navbar').innerHTML = data;
-        })
-        .catch(error => console.error(`Error loading ${'./navbar.html'}:`, error));
+const showMore = (elementId, pointsContainerId) => {
+
+    let points = document.getElementById(pointsContainerId);
+    let moreText = document.getElementById(elementId);
+    let showMoreButton = document.getElementById("show__more")
+
+    if (points.style.display === "none") {
+        moreText.style.display = "none";
+        points.style.display = "inline";
+        showMoreButton.style.display = "inline";
+    }
+    else {
+        moreText.style.display = "inline";
+        points.style.display = "none";
+        showMoreButton.style.display = "none";
+    }
 }
